bffe72b95c9d9810cfb1d5a1528553959bef88ee
Kacper Słomiany <kacper.slomiany@softiq.pl>
2024-12-23 15:56:10 +0100

    first commit

---
+++ src/test/java/com/req1964/mini_library/MiniLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.req1964.mini_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
